db1f0f9b8a63eda16cb462397a1e74ea7d1bf252
Austin Wheeler <austinskullfinder@gmail.com>
2024-08-09 19:12:45 -0400

    Updated Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://awheelr.github.io',
-    base: 'aw-portfolio',
+    site: 'https://austinwheeler.net',
   })
